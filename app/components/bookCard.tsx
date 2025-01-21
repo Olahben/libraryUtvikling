988ea096f7b82cc2a37362b1ccb9581e3d88d672
Ola Hagen Bendixen <olaben@afk.no>
2025-01-21 12:49:31 +0100

    Add BookCard component and integrate it into the homepage for book display

---
+++ app/components/bookCard.tsx
@@ -0,0 +1,58 @@
+import { prisma } from '@/lib/prisma';
+import React from 'react';
+import Image from 'next/image';
+
+interface BookCardProps {
+    name: string;
+    shortDescription: string;
+    imageUrl: string;
+    author:string;
+    publishedAt: Date;
+    genreId: number;
+    thematicKeywordIds: number[];
+}
+
+const BookCard: React.FC<BookCardProps> = ({ name, shortDescription, imageUrl, author, publishedAt, genreId, thematicKeywordIds }) => {
+    const genre = prisma.genre.findUnique({
+        where: { id: genreId }
+    }).then((genre) => {
+        return genre?.name;
+    });
+    const thematicKeyWords = Promise.all(
+        thematicKeywordIds.map(async (id) => {
+            const keyword = await prisma.thematicKeyword.findUnique({
+                where: { id }
+            });
+            return keyword?.name;
+        })
+    );
+
+    return (
+        <div className="card bg-[var(--background)] shadow-xl flex flex-row">
+            <div className="card-body p-2 max-w-[300px]">
+                    <div className="flex w-full items-baseline">
+                        <h2 className="font-sans font-semibold">{name}</h2>
+                        <p className="font-sans text-start">, By {author}, released {publishedAt.getFullYear()}</p>
+
+                    </div>
+                    <p className="font-sans text-[15px]">{shortDescription} ({genre})</p>
+                    <div>
+                    <p className="text-base font-semibold">Categorized as:</p>
+                    {thematicKeyWords.then((keywords) => (
+                <ul>
+                    {keywords.map((keyword, index) => (
+                        <li className="text-[15px]" key={index}>{keyword}</li>
+                    ))}
+                </ul>
+            ))}
+            </div>
+            </div>
+            <figure>
+                {/* Placeholder image  format 0.6  */}
+                <Image src={imageUrl} width={150} height={250} alt="Book title" />
+            </figure>
+        </div>
+    );
+};
+
+export default BookCard;
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,31 @@
-export default function Home() {
+import { prisma } from "@/lib/prisma";
+import BookCard from "./components/bookCard";
+
+export default async function Home() {
+const books = prisma.book.findMany();
+
   return (
-   <button className="btn btn-primary">test</button>
+  <div className="flex p-2 bg-white flex-col">
+    <div className="flex">
+    <p className="text-4xl font-sans font-semibold text-primary">Bibliotek applikasjon</p>
+    </div>
+    <div className="flex">
+      <ul>
+        {(await books).map((book) => (
+          <li className="" key={book.id}>
+            <BookCard
+              name={book.name}
+              author={book.author}
+              publishedAt={book.publishedAt}
+              genreId={book.genreId}
+              imageUrl={book.imageUrl}
+              shortDescription={book.shortDescription}
+              thematicKeywordIds={book.thematicKeywordId}
+            />
+          </li>
+        ))}
+      </ul>
+    </div>
+  </div>
   );
 }
