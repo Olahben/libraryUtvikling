63ecc4c3196a867a0e64dfd8e05553e95bc14449
Ola Hagen Bendixen <olaben@afk.no>
2025-01-21 12:48:53 +0100

    Configure image domains and integrate daisyui themes in Tailwind CSS

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import daisyui from "daisyui";
 
 export default {
   content: [
@@ -6,13 +7,23 @@ export default {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+  plugins: [daisyui],
+  daisyui: {
+    themes: [
+      {
+        light: {
+          ...import("daisyui/src/theming/themes").then((themes) => themes["light"]),
+          primary: "#3f418d",
+          secondary: "#f9eded",
+          accent: "#37cdbe",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+          info: "#2094f3",
+          success: "#009485",
+          warning: "#ff9900",
+          error: "#ff5724",
+        },
       },
-    },
+    ],
   },
-  plugins: [],
 } satisfies Config;
