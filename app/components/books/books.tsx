8e37e4e682cd3f612b79ac05b9141172f3db5525
Ola Hagen Bendixen <olaben@afk.no>
2025-01-22 09:15:46 +0100

    Refactor Books component to fetch detailed book data and pass it to BookCard

--- app/components/books/books.tsx
+++ app/components/books/books.tsx
@@ -2,22 +2,51 @@ import React from 'react';
 import { prisma } from '@/lib/prisma';
 import BookCard from '../bookCard';
 
+interface BookCardProps {
+    name: string;
+    shortDescription: string;
+    imageUrl: string;
+    author:string;
+    publishedAt: Date;
+    genre: string;
+    thematicKeywords: string[];
+}
+
 export default async function Books() {
 const books = await prisma.book.findMany();
+const bookCards: BookCardProps[] = await Promise.all(
+    books.map(async (book) => {
+        const genre = await prisma.genre.findUnique({ where: { id: book.genreId } });
+        const thematicKeywords = await prisma.thematicKeyword.findMany({
+            where: { id: { in: book.thematicKeywordId } },
+        });
+
+        return {
+            name: book.name,
+            author: book.author,
+            publishedAt: book.publishedAt,
+            genre: genre?.name || '',
+            imageUrl: book.imageUrl,
+            shortDescription: book.shortDescription,
+            thematicKeywords: thematicKeywords.map((keyword) => keyword.name),
+        };
+    })
+);
+
 
   return (
     <div className="flex px-3">
         <ul>
-        {(books).map((book) => (
-          <li className="" key={book.id}>
+        {(bookCards).map((book) => (
+          <li className="" key={book.name}>
             <BookCard
               name={book.name}
               author={book.author}
               publishedAt={book.publishedAt}
-              genreId={book.genreId}
+              genre={book.genre}
               imageUrl={book.imageUrl}
               shortDescription={book.shortDescription}
-              thematicKeywordIds={book.thematicKeywordId}
+              thematicKeywords={book.thematicKeywords}
             />
           </li>
         ))}
