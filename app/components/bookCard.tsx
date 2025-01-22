41a48ede24a8157074d059a0743ce97a1b9af837
Ola Hagen Bendixen <olaben@afk.no>
2025-01-22 08:55:22 +0100

    Enhance BookCard component with improved hover effects and code cleanup

--- app/components/bookCard.tsx
+++ app/components/bookCard.tsx
@@ -28,28 +28,26 @@ const BookCard: React.FC<BookCardProps> = ({ name, shortDescription, imageUrl, a
     );
 
     return (
-        <div className="card bg-[var(--background)] shadow-xl flex flex-row">
+        <div className="card bg-[var(--background)] shadow-xl flex flex-row transition-transform duration-300 hover:scale-105 hover:shadow-2xl">
             <div className="card-body p-2 max-w-[300px]">
-                    <div className="flex w-full items-baseline">
-                        <h2 className="font-sans font-semibold">{name}</h2>
-                        <p className="font-sans text-start">, By {author}, released {publishedAt.getFullYear()}</p>
-
-                    </div>
-                    <p className="font-sans text-[15px]">{shortDescription} ({genre})</p>
-                    <div>
-                    <p className="text-base font-semibold">Categorized as:</p>
-                    {thematicKeyWords.then((keywords) => (
+            <div className="flex w-full items-baseline">
+                <h2 className="font-sans font-semibold">{name}</h2>
+                <p className="font-sans text-start">, By {author}, released {publishedAt.getFullYear()}</p>
+            </div>
+            <p className="font-sans text-[15px]">{shortDescription} ({genre})</p>
+            <div>
+                <p className="text-base font-semibold">Categorized as:</p>
+                {thematicKeyWords.then((keywords) => (
                 <ul>
                     {keywords.map((keyword, index) => (
-                        <li className="text-[15px]" key={index}>{keyword}</li>
+                    <li className="text-[15px]" key={index}>{keyword}</li>
                     ))}
                 </ul>
-            ))}
+                ))}
             </div>
             </div>
             <figure>
-                {/* Placeholder image  format 0.6  */}
-                <Image src={imageUrl} width={150} height={250} alt="Book title" />
+            <Image src={imageUrl} width={150} height={250} alt="Book title" />
             </figure>
         </div>
     );
