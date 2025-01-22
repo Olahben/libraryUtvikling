4e5880803611028576d67f855fdab4df77905178
Ola Hagen Bendixen <olaben@afk.no>
2025-01-22 21:09:52 +0100

    Add createBook action to handle book creation with necessary fields

--- app/actions/books.ts
+++ app/actions/books.ts
@@ -1,5 +1,6 @@
 "use server";
 import { prisma } from '@/lib/prisma';
+import { Book } from "../models/book";
 
 export async function deleteBook(id:number) {
     console.log("Deleting book with id: ", id);
@@ -13,5 +14,26 @@ export async function deleteBook(id:number) {
     } catch (error) {
         console.log(error);
     return { success: false, error: error };
+    }
+}
+
+export async function createBook(book:Book) {
+    console.log(book);
+    try {
+        await prisma.book.create({
+            data: {
+                name: book.name,
+                shortDescription: book.shortDescription,
+                imageUrl: book.imageUrl,
+                author: book.author,
+                publishedAt: book.publishedAt,
+                createdAt: new Date(),
+                updatedAt: new Date(),
+                genre: book.genre,
+                thematicKeywords: book.thematicKeywords,
+            },
+        });
+    } catch (error) {
+        console.log(error);
     }
 }
