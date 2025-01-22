9c5ca9e5c83473b801e65fe8cdbf85a5460788f3
Ola Hagen Bendixen <olaben@afk.no>
2025-01-22 08:55:17 +0100

    Refactor Home component to use Books component for book listing

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,34 @@
-import { prisma } from "@/lib/prisma";
-import BookCard from "./components/bookCard";
+import Books from "./components/books/books";
 
-export default async function Home() {
-const books = prisma.book.findMany();
+export default function Home() {
+// const [isMarking, setIsMarking] = useState(false);
+
+// const handleMark = () => {
+//   setIsMarking(!isMarking);
+// };
 
   return (
   <div className="flex p-2 bg-white flex-col">
-    <div className="flex">
+    <div className="flex flex-col mb-6">
     <p className="text-4xl font-sans font-semibold text-primary">Bibliotek applikasjon</p>
+    <div className="flex gap-x-2">
+      <div className="mt-2 btn btn-primary w-fit uppercase transition-transform duration-300 ease-in-out transform hover:scale-105">Add book</div>
+      {/* {isMarking ? (
+        <div
+        onClick={handleMark}
+        className="mt-2 btn btn-secondary text-black w-fit uppercase transition-transform duration-300 ease-in-out transform hover:scale-105">
+        Mark
+      </div>
+      ) : (
+        <div
+        onClick={handleMark}
+        className="mt-2 btn btn-secondary text-black w-fit uppercase transition-transform duration-300 ease-in-out transform hover:scale-105">
+        Cancel marking
+      </div>
+      )} */}
     </div>
-    <div className="flex">
-      <ul>
-        {(await books).map((book) => (
-          <li className="" key={book.id}>
-            <BookCard
-              name={book.name}
-              author={book.author}
-              publishedAt={book.publishedAt}
-              genreId={book.genreId}
-              imageUrl={book.imageUrl}
-              shortDescription={book.shortDescription}
-              thematicKeywordIds={book.thematicKeywordId}
-            />
-          </li>
-        ))}
-      </ul>
     </div>
+    <Books />
   </div>
   );
 }
