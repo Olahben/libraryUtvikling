b7b18a4928da3ac861b5c14256dae5971568cb87
Ola Hagen Bendixen <olaben@afk.no>
2025-01-24 09:05:53 +0100

    Update UpdateBookModal to use initial book data for editing

--- app/components/modals/updateBookModal.tsx
+++ app/components/modals/updateBookModal.tsx
@@ -9,16 +9,7 @@ interface UpdateBookModalProps {
 }
 
 const UpdateBookModal: React.FC<UpdateBookModalProps> = ({ book }) => {
-    const [updatedBook, setBook] = React.useState<Book>({
-        id: 0,
-        name: "",
-        shortDescription: "",
-        imageUrl: "",
-        author: "",
-        publishedAt: new Date(),
-        genre: "Fiction",
-        thematicKeywords: [],
-    });
+    const [updatedBook, setBook] = React.useState<Book>(book);
 
     
     function handleInputChange(event: React.ChangeEvent<HTMLInputElement>) {
@@ -53,37 +44,37 @@ const UpdateBookModal: React.FC<UpdateBookModalProps> = ({ book }) => {
               <label className="label" htmlFor="name">
                 <span className="label-text">Name</span>
               </label>
-              <input required type="text" id="name" className="input input-bordered" value={book.name} onChange={handleInputChange} />
+              <input required type="text" id="name" className="input input-bordered" value={updatedBook.name} onChange={handleInputChange} />
             </div>
             <div className="form-control">
               <label className="label" htmlFor="author">
                 <span className="label-text">Author</span>
               </label>
-              <input required type="text" id="author" className="input input-bordered" value={book.author} onChange={handleInputChange} />
+              <input required type="text" id="author" className="input input-bordered" value={updatedBook.author} onChange={handleInputChange} />
             </div>
           <div className="form-control">
               <label className="label" htmlFor="description">
                   <span className="label-text">Description</span>
               </label>
-              <textarea required id="shortDescription" value={book.shortDescription} onChange={handleInputChange} className="textarea textarea-bordered"></textarea>
+              <textarea required id="shortDescription" value={updatedBook.shortDescription} onChange={handleInputChange} className="textarea textarea-bordered"></textarea>
           </div>
           <div className="form-control">
               <label className="label" htmlFor="name">
                 <span className="label-text">Image Url (Only Norli URLs allowed)</span>
               </label>
-              <input required type="text" id="imageUrl" className="input input-bordered" value={book.imageUrl} onChange={handleInputChange} />
+              <input required type="text" id="imageUrl" className="input input-bordered" value={updatedBook.imageUrl} onChange={handleInputChange} />
             </div>
             <div className="form-control">
               <label className="label" htmlFor="publishedAt">
                 <span className="label-text">Published At</span>
               </label>
-              <input required type="date" value={book.publishedAt.toISOString().split('T')[0]} id="publishedAt" className="input input-bordered" onChange={handleDateInputChange} />
+              <input required type="date" value={updatedBook.publishedAt.toISOString().split('T')[0]} id="publishedAt" className="input input-bordered" onChange={handleDateInputChange} />
             </div>
           <div className="form-control">
             <label className="label" htmlFor="genre">
               <span className="label-text">Genre</span>
             </label>
-            <select value={book.genre} required id="genre" className="select select-bordered" onChange={handleInputChange} >
+            <select value={updatedBook.genre} required id="genre" className="select select-bordered" onChange={handleInputChange} >
               <option value="fiction">Fiction</option>
               <option value="non-fiction">Non-Fiction</option>
               <option value="mystery">Mystery</option>
@@ -101,14 +92,14 @@ const UpdateBookModal: React.FC<UpdateBookModalProps> = ({ book }) => {
               <label className="label" htmlFor="name">
                 <span className="label-text">Thematic keywords, comma separated</span>
               </label>
-              <input required type="text" id="thematicKeywords" className="input input-bordered" value={book.thematicKeywords} onChange={handleThematicKeywordsChange} />
+              <input required type="text" id="thematicKeywords" className="input input-bordered" value={updatedBook.thematicKeywords} onChange={handleThematicKeywordsChange} />
             </div>
           </form>
       
           <div className="modal-action">
             <form onSubmit={handleSubmit} method="dialog" className="w-full">
               {/* if there is a button in form, it will close the modal */}
-              <button className="btn btn-primary w-full">Create</button>
+              <button className="btn btn-primary w-full">Update</button>
             </form>
           </div>
         </div>
